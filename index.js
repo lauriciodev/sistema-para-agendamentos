a99ad25340248fcead66d174cad1f866f43c0d82
LauricioX <developerwebmasterpro@gmail.com>
2022-12-14 21:32:49 -0300

    correções

--- index.js
+++ index.js
@@ -2,24 +2,22 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
-//usando arquivos staticos
 app.use(express.static("public"));
 
-//url encoded
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json);
+app.use(express.json());
 
 app.set("view engine", "ejs");
 mongoose.set("strictQuery", true);
-mongoose.connect("mongodb://localhost:27017/agendamento", {
+mongoose.connect("mongodb://localhost:27017/agendamentos", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 app.get("/", (req, res) => {
-  res.send("teste");
+  res.send("Oi!");
 });
 
-app.listen(3000, () => {
-  console.log("servidor iniciado");
+app.listen(8080, () => {
+  console.log("online");
 });
